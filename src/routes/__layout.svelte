15b0f372ec62b22ad851bb189ad7ef0eff9b9422
ssssota <tommysouta@gmail.com>
2022-04-06 00:55:51 +0900

    set meta info

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,6 +12,16 @@
   export let renderedAt: string;
 </script>
 
+<svelte:head>
+  <title>ssssota</title>
+  <meta name="description" content="ssssota's portfolio" />
+  <meta property="og:title" content="ssssota" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://ssssota.github.io/" />
+  <meta property="og:image" content="https://ssssota.github.io/ogp.png" />
+  <meta property="og:site_name" content="ssssota" />
+</svelte:head>
+
 <main>
   <Card --bg-color="#f9f9f3">
     <slot />
