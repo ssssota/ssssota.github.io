dcf04d803d0e3faaad5b6943268a105e3f395306
ssssota <tommysouta@gmail.com>
2022-04-07 19:10:39 +0900

    add twitter card type

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -20,6 +20,7 @@
   <meta property="og:url" content="https://ssssota.github.io/" />
   <meta property="og:image" content="https://ssssota.github.io/ogp.png" />
   <meta property="og:site_name" content="ssssota" />
+  <meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 
 <main>
