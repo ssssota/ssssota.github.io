093a39124bc98ead6a8130e678db114edff805b7
ssssota <tommysouta@gmail.com>
2022-12-24 18:20:42 +0900

    enable gfm

--- src/lib/components/Description.svelte
+++ src/lib/components/Description.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Markdown from 'svelte-exmarkdown';
+  import { gfmPlugin } from 'svelte-exmarkdown/gfm';
   export let content: string;
 </script>
 
-<Markdown md={content} />
+<Markdown md={content} plugins={[gfmPlugin]} />
