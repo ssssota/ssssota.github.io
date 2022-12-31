d8f6e1b635da8a7a8bead0425391d59138c1d44d
ssssota <tommysouta@gmail.com>
2022-12-31 22:56:11 +0900

    fix env var

--- src/routes/articles/+page.server.ts
+++ src/routes/articles/+page.server.ts
@@ -1,8 +1,8 @@
-import { GITHUB_REPO, GITHUB_TOKEN } from '$env/static/private';
+import { GITHUB_REPOSITORY, GITHUB_TOKEN } from '$env/static/private';
 import { Client } from '$lib/data/client';
 import type { PageServerLoad } from './$types';
 
-const [owner, name] = GITHUB_REPO.split('/');
+const [owner, name] = GITHUB_REPOSITORY.split('/');
 
 export const load = (async () => {
   const client = new Client(GITHUB_TOKEN, {
