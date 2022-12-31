dc3a860ad6baa1e74a9ae3640d48890332dcac57
ssssota <tommysouta@gmail.com>
2022-12-31 23:40:16 +0900

    permission check

--- src/routes/articles/+page.server.ts
+++ src/routes/articles/+page.server.ts
@@ -3,7 +3,6 @@ import { Client } from '$lib/data/client';
 import type { PageServerLoad } from './$types';
 
 const [owner, name] = GITHUB_REPOSITORY.split('/');
-console.log({ GITHUB_REPOSITORY, owner, name });
 
 export const load = (async () => {
   const client = new Client(GITHUB_TOKEN, {
