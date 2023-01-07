4d73bb61c32dd190b8cd068d24f66814b9ea8791
ssssota <tommysouta@gmail.com>
2023-01-07 13:17:14 +0900

    meta

--- src/routes/(portfolio)/+layout.svelte
+++ src/routes/(portfolio)/+layout.svelte
@@ -6,6 +6,17 @@
   export let data: PageData;
 </script>
 
+<svelte:head>
+  <title>ssssota</title>
+  <meta name="description" content="ssssota's portfolio" />
+  <meta property="og:title" content="ssssota" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://ssssota.dev/" />
+  <meta property="og:image" content="https://ssssota.dev/ogp.png" />
+  <meta property="og:site_name" content="ssssota" />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <main>
   <Card --bg-color="#f9f9f3">
     <slot />
--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -6,6 +6,17 @@
   export let data: PageData;
 </script>
 
+<svelte:head>
+  <title>{data.article.title} - ssssota</title>
+  <meta name="description" content={data.article.description} />
+  <meta property="og:title" content={data.article.title} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://ssssota.dev/" />
+  <meta property="og:image" content="https://ssssota.dev/ogp.png" />
+  <meta property="og:site_name" content="ssssota" />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <article>
   <h1>{data.article.title}</h1>
   <small>{new Date(data.article.createdAt).toISOString()}</small>
